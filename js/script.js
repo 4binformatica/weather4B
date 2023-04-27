a951ee4da07ffdaa3bb51446815285e85f526d29
ZioJis <andrea.mercati0@gmail.com>
2023-04-27 21:15:35 +0200

    added the appears Text

--- js/script.js
+++ js/script.js
@@ -79,6 +79,16 @@ function suggestCity(query) {
 var search = function () {
   //reset leaflet map and layer before searching
 
+  // Rimuove la classe "hidden" dalla sezione del meteo con un'animazione di fade
+  const weatherSection = document.querySelector('#containerAll');
+  weatherSection.style.opacity = 0;
+  weatherSection.classList.remove('hidden');
+  setTimeout(() => {
+    weatherSection.style.opacity = 1;
+  }, 300);
+  
+  // Aggiunge la classe "hidden" alla sezione dei suggerimenti
+  document.querySelector('#appearsText').classList.add('hidden');
 
 
   console.log("searching");
